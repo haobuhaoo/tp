560fc403a5f4762abae3ea3598bf86b8f720a3a7
haobuhaoo <haoyi0509@gmail.com>
2025-10-30 22:36:34 +0800

    Fix bug

--- src/test/java/seedu/address/logic/commands/ParticipationCommandPersistenceTest.java
+++ src/test/java/seedu/address/logic/commands/ParticipationCommandPersistenceTest.java
@@ -121,7 +121,6 @@ public class ParticipationCommandPersistenceTest {
         @Override public ObservableList<Reminder> getFilteredReminderList() {
             return FXCollections.observableArrayList();
         }
-        @Override public void refreshReminders() { }
         @Override public void updateFilteredReminderList(Predicate<Reminder> predicate) { }
         @Override public boolean hasGroup(GroupName name) {
             return false;
